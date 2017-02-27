a0a963a084c8dfc1b6ba2187edb4916ae5321ff8
Nik <nnonexistent@gmail.com>
2017-02-27 17:16:41 +0300

    Unify paths

--- hub.sh
+++ hub.sh
@@ -8,7 +8,7 @@ docker rm -vf jenkins
 
 # Run container
 docker run -dt -p 127.0.0.1:49001:8080 \
-               -v /var/jenkins:/var/jenkins_home \
+               -v /var/jenkins:/var/jenkins \
                -v /var/run/docker.sock:/var/run/docker.sock \
                --restart always \
                --name jenkins \
