169ff5ac7931924585031b248c0344507ec9842e
Nik <nnonexistent@gmail.com>
2017-03-02 21:28:21 +0300

    Switch to named volumes from filesystem mount for data storage

--- local.sh
+++ local.sh
@@ -9,7 +9,7 @@ docker rm -vf jenkins
 
 # Run container
 docker run -dt -p 127.0.0.1:49001:8080 \
-               -v /var/jenkins_home:/var/jenkins_home \
+               -v jenkins_home:/var/jenkins_home \
                -v /var/run/docker.sock:/var/run/docker.sock \
                --restart always \
                --name jenkins \
